5e18311da3d6b91f904d40a191943ba25ed3f768
Corey Osman <corey@nwops.io>
2016-09-15 22:48:28 -0400

    updates to scripts and adds presentation pdf

--- demo_scripts/17_remote_classification.sh
+++ demo_scripts/17_remote_classification.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 docker-compose up -d
 container=`docker ps |grep puppet/puppetserver |cut -f1 -d' '`
-# docker cp ./demo_scripts/repl_scripts/auth.conf ${container}:/etc/puppetlabs/puppetserver/conf.d/auth.conf
-# docker cp ./demo_scripts//repl_scripts/puppetserver.conf ${container}:/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf
+docker cp ./demo_scripts/repl_scripts/auth.conf ${container}:/etc/puppetlabs/puppetserver/conf.d/auth.conf
+docker cp ./demo_scripts//repl_scripts/puppetserver.conf ${container}:/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf
 docker exec -ti ${container} /opt/puppetlabs/puppet/bin/gem install puppet-repl
-# docker-compose restart puppet
-# docker-compose exec puppet bash
-# docker-compose run repl_agent bash
+docker-compose restart puppet
+docker-compose exec puppet bash
+docker-compose run repl_agent bash
